ca54f67012efc769a585c1e3e2e1801fbdb3a0a3
Ravi Sabloak <ravisabloak@Ravis-MacBook-Pro.local>
2014-10-26 15:49:52 +0700

    Initial Commit

---
+++ Git Test/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  Git Test
+//
+//  Created by Ravi Sabloak on 26/10/14.
+//  Copyright (c) 2014 Ravi Sabloak. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
