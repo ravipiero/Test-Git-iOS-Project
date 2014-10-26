f44046a1bc4eeeaab6286561157c87e386847044
Ravi Sabloak <ravisabloak@Ravis-MacBook-Pro.local>
2014-10-26 16:02:02 +0700

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Ravi Sabloak on 26/10/14.
+//  Copyright (c) 2014 Ravi Sabloak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
